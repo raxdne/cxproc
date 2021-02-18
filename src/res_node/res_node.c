85e0bca54710a46d417e8c8d561cbcda0582ea12
A. Tenbusch <raxdne@web.de>
2021-02-18 20:41:41 +0100

    minor update for fxpath locating in pie text

--- src/res_node/res_node.c
+++ src/res_node/res_node.c
@@ -2735,7 +2735,6 @@ resNodeContentToDOM(xmlNodePtr pndArg, resNodePtr prnArg)
 	  RecognizeUrls(pndPie);
 	  RecognizeDates(pndPie, iMimeType);
 	  RecognizeTasks(pndPie);
-	  domSetPropFileXpath(pndPie,BAD_CAST"fxpath",NULL);
 	  RecognizeHashtags(pndPie,NULL, NULL);
 	  RecognizeGlobalTags(pndTags, pndPie);
 	  CleanListTag(pndTags, FALSE);
