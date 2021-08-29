2cc84ef449ff91f618fec94adf3cb7c4dfc75bae
A. Tenbusch <raxdne@web.de>
2021-08-29 16:59:35 +0200

    minor update (update of CMake config for libcurl and libarchive on win64)

--- misc/prepare-cmake.bat
+++ misc/prepare-cmake.bat
@@ -1,10 +1,10 @@
 REM
-REM (p) 2020 A. Tenbusch
+REM (p) 2020,2021 A. Tenbusch
 REM
 
 for /f "delims=" %%i in ("%0") do set CXPBASE=%%~dpi
 
-SET STATIC=0
+SET STATIC=1
 SET FLAG64=1
 
 REM VC++ Environment
@@ -20,14 +20,16 @@ IF "%STATIC%" == "1" (
   SET ARCH=%VSCMD_ARG_TGT_ARCH%-windows
 )
 
-pushd C:\UserData\Develop\vcpkg
-REM bootstrap-vcpkg.bat -win64 -disableMetrics
-.\vcpkg integrate install
+IF "0" == "1" (
+  pushd C:\UserData\Develop\vcpkg
+  REM bootstrap-vcpkg.bat -win64 -disableMetrics
+  .\vcpkg integrate install
 
-.\vcpkg --triplet %ARCH% --x-install-root=%CXPBASE%..\.. install zlib liblzma pcre2 libxml2 libxslt libexif sqlite3 duktape
+  .\vcpkg --triplet %ARCH% --x-install-root=%CXPBASE%..\.. install curl zlib liblzma libarchive pcre2 libxml2 libxslt libexif sqlite3 duktape
 
-REM curl libarchive imagemagick libgif libjpeg libpng libtiff
-popd
+  REM imagemagick libgif libjpeg libpng libtiff
+  popd
+)
 
 set PREFIX=%CXPBASE%..\..\%ARCH%
 
@@ -58,14 +60,20 @@ REM robocopy %CXPBASE%..\doc %DIR_DOC% *.txt
 REM robocopy /S %CXPBASE%..\xml %PREFIX%\xml
 REM robocopy /S %CXPBASE%..\examples %PREFIX%\examples
 
-pushd %PREFIX%
-REM "C:\UserData\Programme\7-ZipPortable\App\7-Zip\7z.exe" a -r ..\cxproc-v1.3-pre_%ARCH%.zip bin xml doc examples www\cgi-bin www\html\test -x!*.pdb -x!*.ilk -x!cxproc-test.exe
-popd
+REM robocopy /S "%PREFIX%" "C:\UserData\Develop\cxproc-build\%ARCH%" *.*
 
-pushd %CXPBASE%..
-REM "C:\UserData\Programme\cmake\bin\cmake.exe" -B %DIR_BUILD% -G "Visual Studio 16 2019" -A %VSCMD_ARG_TGT_ARCH% -DCMAKE_TOOLCHAIN_FILE=C:\UserData\Develop\vcpkg\scripts\buildsystems\vcpkg.cmake -D CXPROC_DOC:BOOL=OFF -D CXPROC_PIE:BOOL=ON -D CXPROC_EXPERIMENTAL:BOOL=OFF -D CXPROC_CXX:BOOL=OFF
-REM "C:\UserData\Programme\cmake\bin\cmake.exe" --build %DIR_BUILD% --config Release --target cxproc
-popd
+IF "0" == "1" (
+  pushd %PREFIX%
+  REM "C:\UserData\Programme\7-ZipPortable\App\7-Zip\7z.exe" a -r ..\cxproc-v1.3-pre_%ARCH%.zip bin xml doc examples www\cgi-bin www\html\test -x!*.pdb -x!*.ilk -x!cxproc-test.exe
+  popd
+)
+
+IF "0" == "1" (
+  pushd %CXPBASE%..
+  REM "C:\UserData\Programme\cmake\bin\cmake.exe" -B %DIR_BUILD% -G "Visual Studio 16 2019" -A %VSCMD_ARG_TGT_ARCH% -DCMAKE_TOOLCHAIN_FILE=C:\UserData\Develop\vcpkg\scripts\buildsystems\vcpkg.cmake -D CXPROC_DOC:BOOL=OFF -D CXPROC_PIE:BOOL=ON -D CXPROC_EXPERIMENTAL:BOOL=OFF -D CXPROC_CXX:BOOL=OFF
+  REM "C:\UserData\Programme\cmake\bin\cmake.exe" --build %DIR_BUILD% --config Release --target cxproc
+  popd
+)
 
 REM TODO: CPack s. https://cmake.org/cmake/help/latest/module/CPack.html
 
