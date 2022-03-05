3e3d9ff91ee4f4cf3d320355f3b05e6988221c1e
A. Tenbusch <raxdne@web.de>
2022-03-05 10:20:59 +0100

    minor update (script testing)

--- src/option/json/test/test_cxp_json.c
+++ src/option/json/test/test_cxp_json.c
@@ -31,25 +31,6 @@ jsonCxpTest(cxpContextPtr pccArg)
   n_ok=0;
   i=0;
 
-#if 0
-  /* TEST: 
-   */
-  i++;
-  printf("TEST %i in '%s:%i': ",i,__FILE__,__LINE__);
-  getXmlBody("<abc/>",&iA,&iB);
-  if (iA==0 && iB==5) {
-	n_ok++;
-	printf("OK\n");
-  }
-  else {
-    printf("ERROR\n");
-  }
-
-
-  printf("Result in '%s': %i/%i OK\n\n",__FILE__,n_ok,i);
-#else
-  printf("Skip TEST in '%s'\n\n",__FILE__);
-#endif
 
   return (i - n_ok);
 }
