8a1916ef1a1d48248d75b7ee3e33f0e125cb1db2
A. Tenbusch <raxdne@web.de>
2022-07-03 16:40:15 +0200

    some cmake optimizations

--- cmake/docs.cmake
+++ cmake/docs.cmake
@@ -5,17 +5,11 @@
 FIND_PACKAGE(Doxygen)
 
 if (DOXYGEN_FOUND)
-  option(CXPROC_DOC "Build documentation" OFF)
-else (DOXYGEN_FOUND)
-  message("Doxygen need to be installed to generate the doxygen documentation")
-endif (DOXYGEN_FOUND)
-
-if (CXPROC_DOC)
-  # note the option ALL which allows to build the docs together with the application
-  add_custom_target( doc_doxygen ALL
+  add_custom_target( doc_doxygen
     COMMAND DOXYGEN_NAME=${CMAKE_PROJECT_NAME} DOXYGEN_OUTPUT=${PROJECT_BINARY_DIR}/.. ${DOXYGEN_EXECUTABLE} ${PROJECT_SOURCE_DIR}/misc/Doxyfile
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
     COMMENT "Generating API documentation with Doxygen"
     VERBATIM )
-endif ()
-
+else (DOXYGEN_FOUND)
+  message("Doxygen need to be installed to generate the doxygen documentation")
+endif (DOXYGEN_FOUND)
