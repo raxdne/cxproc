b08e11e538213696a1c8c687bec39de98326077e
A. Tenbusch <raxdne@web.de>
2025-12-20 23:00:16 +0100

    update of test configs

--- test/test-all.sh
+++ test/test-all.sh
@@ -4,6 +4,6 @@
 
 for D in calendar dir each plain system xhtml xml xsl
 do
-    (cd $D && valgrind --tool=memcheck --leak-check=full --fullpath-after= --num-callers=20 ~/cxproc-build/x86_64-gnu-linux/bin/cxproc config.cxp 2> result/test-$D.log)
+    (cd $D && mkdir result && valgrind --tool=memcheck --leak-check=full --fullpath-after= --num-callers=20 ~/cxproc-build/x86_64-gnu-linux/bin/cxproc config.cxp 2> result/test-$D.log)
 done
 
